060759ef45c37f468df7aa56063ac0e5be345946
Sundish <caliduz@gmail.com>
2021-04-13 13:56:30 -0500

    added save and update for cache

--- imagegrab.sh
+++ imagegrab.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 # for artist name: https://www.wikiart.org/en/artist_name
-# for movment: https://www.wikiart.org/en/paintings-by-style/movement_here?select=featured#!%23filterName:featured,viewType:masonry
+# for movement: https://www.wikiart.org/en/paintings-by-style/movement_here?select=featured#!%23filterName:featured,viewType:masonry
 
+image_dir="/home/$USER/" # folder for save images
 cache_dir="/home/$USER/.cache/artgrabber"
+
 REGEX='https://uploads\d\.wikiart\.org/\d*?/?images/(?!pixel-icons)[\w|-]+/\w+[-\w|\d?]+?\.jpg'
+DEL=1
+
 if [ ! -d $cache_dir ]; then
 	mkdir "$cache_dir"
 	echo "cache directory created at /home/$user/.cache"
 
+fi
+
+if [ $# -gt 0 ]; then
+	case $1 in
+		-u) # update the cache with the urls.
+			rm "$cache_dir/home.urls" 2> /dev/null;;
+		-s) # don't delete image, save it.
+			DEL=0
+
+	esac
 fi
 
 if [ ! -f "$cache_dir/home.urls" ]; then
@@ -20,5 +34,6 @@ SEL="$(shuf "$cache_dir/home.urls" | head -1)"
 OUT_NAME="${SEL##*/}" #name of the output file
 
 echo "Downloading $OUT_NAME ..."
-wget --quiet $SEL && sxiv -b "$OUT_NAME" && rm "$OUT_NAME"
-echo "Removing $OUT_NAME ..."
+wget --quiet --directory-prefix="$image_dir" $SEL && sxiv -b "$image_dir/$OUT_NAME"
+
+[ $DEL -eq 1 ] && (echo "Removing $image_dir/$OUT_NAME ..." && rm "$image_dir/$OUT_NAME")
